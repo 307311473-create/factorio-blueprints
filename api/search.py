44967c92585837046ce453fba037ea5deef702a6
307311473-create <307311473@qq.com>
2025-12-01 04:02:28 +0800

    Update search.py

--- api/search.py
+++ api/search.py
@@ -1,2 +1,8 @@
 def handler(request):
-    return {"statusCode": 200, "body": "OK"}
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "text/plain"
+        },
+        "body": "Hello from Vercel!"
+    }
