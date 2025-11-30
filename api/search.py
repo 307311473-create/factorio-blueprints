722ee9203e916e3cf641a4648e191893cce503ea
307311473-create <307311473@qq.com>
2025-12-01 03:14:58 +0800

    Update search.py

--- api/search.py
+++ api/search.py
@@ -1,11 +1,8 @@
-import json
-
 def handler(request):
-    response = {
-        "message": "API is working!"
-    }
     return {
-        'statusCode': 200,
-        'headers': {'Content-Type': 'application/json'},
-        'body': json.dumps(response, ensure_ascii=False)
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": '{"status": "ok", "message": "Function loaded successfully"}'
     }
