364fafc8bc3d7731205eb647b69e4eb717daca84
307311473-create <307311473@qq.com>
2025-12-01 04:10:05 +0800

    Update search.py

--- api/search.py
+++ api/search.py
@@ -1,8 +1,5 @@
 def handler(request):
     return {
         "statusCode": 200,
-        "headers": {
-            "Content-Type": "text/plain"
-        },
-        "body": "Hello from Vercel!"
+        "body": "OK"
     }
